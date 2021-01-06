032383542605dfedc021d7975aa96b6fc27045f0
DianeDii <3463600338@qq.com>
2021-01-06 11:08:55 +0800

    后台为文章添加/展示分类信息

--- src/components/background/edit.vue
+++ src/components/background/edit.vue
@@ -7,12 +7,30 @@
       <div id="editor" style="text-align: left; width:60%">
           <!-- {{articledetail[1]}} -->
       </div>
-      <div id="btn">
-          <el-button type="primary" @click="submit">提交</el-button>
+      <div id="sort">
+          <el-button id="btn" type="primary" @click="submit">提交</el-button>
           <el-button type="primary" @click="reset">放弃</el-button>
+          <el-button type="primary" @click="out">自定义调试</el-button>
+          <span id="switch">生活随笔:</span>
+          <el-switch
+            v-model="islife"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+        </el-switch>
+          
       </div>
-      
-                  
+      <div id="select" v-if="!islife">
+          <span>详细分类：</span>
+            <el-select v-model="sortchoose" placeholder="请选择">
+                <el-option
+                v-for="item in sortlist"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+                </el-option>
+            </el-select>
+      </div>
+
   </div>
 </template>
 
@@ -34,29 +52,68 @@ export default {
                 title: String,
                 summary: String,
                 content: String
-            }//构造文章返回数据
+            },//构造文章返回数据
+            islife: false,
+            sortlist: [],
+            sortchoose: '',
+            isnewArtid: Number,
+            sortdata: null //文章分类提交数据
         }
     },
     methods:{
-        submit(){
+        out(){
+
+
+        
+        },
+        async submit(){
+            if (this.islife === false && this.sortchoose == '') {
+                window.alert("你还未选择分类！");
+            } else {
             this.buildarticle.title = this.inputtitle
             this.buildarticle.summary = "无"
+            // 思路就是：先在create()初始化当前有的分类。并展示，然后再submit()中用axios来进行已选分类的保存，然后后端接口可能有问题的话就是调bug了
+            // 2021-1-5:已选分类的文章，在编辑时应该带上之前选的分类情况，
+            // 还有就是未选择分类（非随笔且未选择其他分类的不允许被提交）ok
+            // 这块整完了看一下文章怎么支持带格式的存数据库
+
+            // Q:多个axios执行顺序问题
+
             
-            
-            console.log(this.buildarticle)
+
             if(this.isnew === "true"){
                 this.buildarticle.content = this.edit.txt.text()
                 // 新建文章
-                this.axios({
+                await this.axios({
                     url:'/blog/init',
                     method: 'post',
                     data: this.buildarticle,
                     headers:{ token:'',client:'' }
                     }).then(res =>{
+                        console.log("提交文章信息")
+                        this.isnewArtid =res.data.data;
+                    // 构造分类提交数据
+                    if(this.islife === true){
+                          this.sortdata ={"sortId":1,"articleId":this.isnewArtid}
+                    }else{
+                          this.sortdata ={"sortId": this.sortchoose,"articleId":this.isnewArtid}
+                     }
+                    },function(error){
+                        console.log(error.res)
+                    })
+     
+                    //  提交分类信息
+                    await this.axios({
+                    url:'/sort/add',
+                    method: 'post',
+                    data: this.sortdata,
+                    headers:{ token:'',client:'' }
+                    }).then(res =>{
+                        console.log("为文章加分类成功")
                         this.$router.push("/backindex")
                     },function(error){
                         console.log(error.res)
-                    }) 
+                    })   
             }else{
                 // 更新文章
                 this.buildarticle.content = this.edit.txt.text()
@@ -68,12 +125,32 @@ export default {
                     headers:{ token:'',client:'' }
                     }).then(res =>{
                         console.log("更新成功")
+                    },function(error){
+                        console.log(error.res)
+                    })
+                    
+                     if(this.islife === true){
+                         this.sortdata ={"sortId":1,"articleId":this.$route.query.id}
+                     }else{
+                         this.sortdata ={"sortId": this.sortchoose,"articleId":this.$route.query.id}
+                }
+                      this.axios({
+                        url:'/sort/add',
+                        method: 'post',
+                        data: this.sortdata,
+                        headers:{ token:'',client:'' }
+                    }).then(res =>{
+                        console.log("为文章加分类成功")
                         this.$router.push("/backindex")
                     },function(error){
                         console.log(error.res)
-                    })  
+                    })
+            }  
             }
+ 
+        
         },
+   
         reset(){
             console.log("exit")
             this.$router.push("/backindex")
@@ -81,7 +158,13 @@ export default {
 
     },
     created () {
-      
+      this.axios.get('/sort/list').then(response=> {
+          this.sortlist = response.data.data;
+          this.sortlist.shift();//删除随笔的分类
+        //   console.log(this.sortlist.data)
+        }).catch(function (error) {
+            console.log(error);
+        });
     },
     mounted(){
         const editor = new E('#editor')
@@ -106,7 +189,19 @@ export default {
                     editor.txt.append(res.data.data[1])
                  },function(error){
                     console.log(error.res)
-                }) 
+                })
+                
+            // 在这加载已定义分类信息
+            this.axios.get('/sort/'+this.$route.query.id).then(res=>{
+                // console.log(res.data.data)
+                if(res.data.data == 1){
+                    this.islife =true
+                }else{
+                    this.sortchoose =res.data.data
+                }
+            }).catch(function(err){
+                console.log(err)
+            })
         }
     }
 
@@ -116,5 +211,17 @@ export default {
 <style>
 #editor{
     margin-left: 20%;
+}
+#sort{
+    position: relative;
+    margin-left: 20%;
+    float: left;
+    height: 100px;
+}
+  .el-select-dropdown {
+     top: 680px !important;
+  }
+#switch{
+    margin-left: 100px;
 }
 </style>
